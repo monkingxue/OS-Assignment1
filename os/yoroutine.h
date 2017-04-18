a89a9431095f34049211ac72fb45a7e4149bd7dd
monkingxue <1360662499@qq.com>
2017-04-18 11:10:27 +0800

    [add] add part of struct of yoroutine

--- os/yoroutine.h
+++ os/yoroutine.h
@@ -1,16 +1,43 @@
 #ifndef OS_YOROUTINE_H
 #define OS_YOROUTINE_H
 
-#include <sys/ucontext.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "define.h"
 #include "scheduler.h"
 
-typedef void (*yc_fn)(Scheduler *, void *ud);
+typedef void (*yc_fn)(void *arg);
 
 class Yoroutine {
+public:
+    yc_fn func;
+    void *arg;
+    char *stack;
+    int status;
+    int id;
 
+    Yoroutine();
+
+    ~Yoroutine();
+
+    int get_status();
+
+    bool set_status();
+
+private:
+    ucontext_t ctx;
+    Scheduler *scheduler;
+    size_t stack_size;
+
+    static void pause(int to_status);
+    static int save_stack();
+
+    void resume(int id);
+
+    void yield() {pause(YOROUTINE_SUSPENDED);}
+
+    void block() {pause(YOROUTINE_BLOCKED);}
 };
 
 #endif //OS_YOROUTINE_H
