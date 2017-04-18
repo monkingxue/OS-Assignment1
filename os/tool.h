8887478d2c3cc4a591d5f14ff12c7223271c89fa
monkingxue <1360662499@qq.com>
2017-04-19 00:07:15 +0800

    [add] add tool file

---
+++ os/tool.h
@@ -0,0 +1,11 @@
+#ifndef OS_TOOL_H
+#define OS_TOOL_H
+
+#include <iostream>
+#include <string>
+
+static void errPrint(std::string s) {
+    std::cout << "Error: " << s << std::endl;
+}
+
+#endif //OS_TOOL_H
